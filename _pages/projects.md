ed7f6331e7f5d34af862e83425cf8d0843ae5471
Renato <renato145@hotmail.com>
2018-05-29 01:29:55 -0500

    some imgs

---
+++ _pages/projects.md
@@ -0,0 +1,11 @@
+---
+title: "Projects"
+permalink: /projects/
+author_profile: true
+# header:
+#     image: "images/something.png"
+---
+
+List of github repos here:
+* llalala1
+* lalal2
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Some post test",
-        "excerpt":"Hello kasdasdasdadssadasd.asdasdasdasdsdasd.     import numpy as np    np.arange(2)asd asd The end :) ","categories": [],
+        "excerpt":"Hello kasdasdasdadssadasd.asdasdasdasdsdasd.     import numpy as np    np.arange(2)asd asd x+t The end :) ","categories": [],
         "tags": ["machine learning","neural networks"],
         "url": "http://localhost:4000/test/",
         "teaser":null}]
