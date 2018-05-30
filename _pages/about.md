673eba2cf849d918b367233d89468cbb35e9b479
Renato <renato145@hotmail.com>
2018-05-30 01:38:19 -0500

    bio

--- _pages/about.md
+++ _pages/about.md
@@ -1,8 +1,15 @@
 ---
 title: "About"
 permalink: /about/
-# header:
-#     image: "images/something.png"
+header:
+    image: "images/bio.jpg"
 ---
 
-Something here.......
+Data scientist and machine learning researcher at the Artificial Intelligence Group at PUCP [(iapucp)](http://ia.inf.pucp.edu.pe/). I studied a MS in Computer Science at the Pontifical Catholic University of Perú (PUCP).
+
+Things I like, in no particular order:
+- Teaching.
+- Movies (Kieślowski, Tarkovsky, Jodorowsky, ...sky).
+- Cooking.
+- Books, a little bit of everything. One particular favorite: The Museum of Eterna's Novel.
+- Camping.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Plotting with altair",
-        "excerpt":"Code for plot: import altair as altfrom vega_datasets import dataalt.renderers.enable('notebook')d = data.iris()brush1 = alt.selection_interval()brush2 = alt.selection_interval()base = alt.Chart(d)p1 = base.mark_point().encode( x='petalLength', y='petalWidth', color=alt.condition(brush2, 'species', alt.value('lightgray'))).properties( selection=brush1, width=300, height=300)p2 = base.mark_point().encode( x='sepalLength', y='sepalWidth', color=alt.condition(brush1, 'species', alt.value('lightgray'))).properties( selection=brush2, width=300, height=300)bars = base.mark_bar().encode( y='species', x='count(species)', color='species').transform_filter( {'and': [brush1.ref(), brush2.ref()]}).properties( width=660)chart = (p1 |...","categories": [],
+        "excerpt":"This library has recently caught my attention: Altair. So here is a test check the interactive behavior on a post, I can’t wait to use it more in here :). Code for the plot: import altair as altfrom vega_datasets import dataalt.renderers.enable('notebook')d = data.iris()brush1 = alt.selection_interval()brush2 = alt.selection_interval()base = alt.Chart(d)p1 =...","categories": [],
         "tags": ["dataviz","altair"],
-        "url": "http://localhost:4000/altair-test/",
-        "teaser":null}]
+        "url": "http://localhost:4000/plotting-with-altair/",
+        "teaser":"http://localhost:4000/images/plotting-with-altair/altair_ico.png"}]
