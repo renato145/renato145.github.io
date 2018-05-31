78b9502317e1d67b023bd6e07962a53de0add621
Renato <renato145@hotmail.com>
2018-05-31 02:29:58 -0500

    criminality 0.1

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"This library has recently caught my attention: Altair. So here is a test check the interactive behavior on a post, I can’t wait to use it more in here :). Code for the plot: import altair as altfrom vega_datasets import dataalt.renderers.enable('notebook')d = data.iris()brush1 = alt.selection_interval()brush2 = alt.selection_interval()base = alt.Chart(d)p1 =...","categories": [],
         "tags": ["dataviz","altair"],
         "url": "http://localhost:4000/plotting-with-altair/",
-        "teaser":"http://localhost:4000/images/plotting-with-altair/altair_ico.png"}]
+        "teaser":"http://localhost:4000/images/plotting-with-altair/altair_ico.png"},{
+        "title": "INEI Criminality Data",
+        "excerpt":"Data obtained from http://criminalidad.inei.gob.pe. As the data is given in number of criminal complaints, ratios were calculated using data from INEI series. Here is a view of the numbers and ratio of complaints by city and type: ","categories": [],
+        "tags": ["dataviz","altair","inei"],
+        "url": "http://localhost:4000/inei-criminality-data/",
+        "teaser":null}]
