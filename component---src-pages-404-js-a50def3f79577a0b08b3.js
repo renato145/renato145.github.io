3433d80e79ce3364b1ae3b995f1c07e41f245d63
Renato <renato145@hotmail.com>
2020-02-11 15:58:00 +1030

    Updates

---
+++ component---src-pages-404-js-a50def3f79577a0b08b3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(t,e,a){"use strict";a.r(e),a.d(e,"pageQuery",(function(){return u}));var n=a("q1tI"),o=a.n(n),i=a("7oih");e.default=function(t){var e=t.data,a=t.location,n=e.site.siteMetadata.title;return o.a.createElement(i.a,{location:a,title:n,headerConfig:{title:"404: Not Found"}},o.a.createElement("h1",null,"Not Found"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))};var u="1097489062"}}]);
+//# sourceMappingURL=component---src-pages-404-js-a50def3f79577a0b08b3.js.map
---
+++ component---src-pages-default-js-64f2b4e5abbb449bcbf9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{o1LO:function(t,e,n){"use strict";n.r(e),n.d(e,"pageQuery",(function(){return r}));var a=n("q1tI"),i=n.n(a),o=(n("Wbzz"),n("7oih"));n("9CUm");e.default=function(t){var e=t.data,n=t.location,a=e.site.siteMetadata.title;return i.a.createElement(o.a,{location:n,title:a,description:"some description"})};var r="1097489062"}}]);
+//# sourceMappingURL=component---src-pages-default-js-64f2b4e5abbb449bcbf9.js.map
