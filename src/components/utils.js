42fc77b48132ddb154e2783d1a74d60001c52976
Renato <renato145@hotmail.com>
2020-02-17 15:33:05 +1030

    add function strJoin

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,3 +1,4 @@
 export const unCamelCase = str => str.replace(/([a-z\xE0-\xFF])([A-Z\xC0\xDF])/g, "$1 $2").toLowerCase();
 export const singleSpace = str => str.replace(/\s+/g, ' ');
 export const titleCase = str => str.toLowerCase().replace(/^\w|\s\w/g, d => d.toUpperCase());
+export const strJoin = (str, separator=', ') => str.reduce((o,acc) => `${o}${separator}${acc}`);
