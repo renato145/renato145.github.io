f2ae1243671676d04502bf01a5a797ec84f46a08
Renato <renato145@hotmail.com>
2020-04-30 19:41:40 +0930

    rm import

--- src/pages/searchGraph.js
+++ src/pages/searchGraph.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import { SearchGraph } from '../components/SearchGraph';
-import styled from 'styled-components';
 
 const SearchGraphPage = ({ location }) => {
   const title = 'Search Graph';
