4a7574b0c8c825514a9e0c00c2366e48dc9a49c8
Renato <renato145@hotmail.com>
2020-05-24 02:17:16 +0930

    refactor

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import TagList from './TagList';
+import styled from 'styled-components';
+
+const PostTitle = styled.h3`
+  font-size: 1.5rem;
+  margin-bottom: 0px;
+`;
 
 const PostPreview = ({ node }) => {
   const postTitle = node.frontmatter.title || node.fields.slug;
   const tags = node.frontmatter.tags;
 
   return (
-    <article className='mb-4'>
-      <header className='mb-1'>
-        <h3 className='mb-0'>
-          <Link to={node.fields.slug}>
-            {postTitle}
-          </Link>
-        </h3>
-        <small className='text-muted'>
+    <article className="mb-4">
+      <header className="mb-1">
+        <PostTitle>
+          <Link to={node.fields.slug}>{postTitle}</Link>
+        </PostTitle>
+        <small className="text-muted">
           {node.frontmatter.date}
-          <TagList tags={tags} prefix=' - ' />
+          <TagList tags={tags} prefix=" - " />
         </small>
       </header>
       <section>
