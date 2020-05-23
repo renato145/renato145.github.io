a1fee32772f009e93590d84e34b9da9a4160732d
Renato <renato145@hotmail.com>
2020-05-24 02:54:09 +0930

    add load more

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PostPreview from './PostPreview';
+import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
 import './Posts.css';
 
-const Posts = ({ title = false, posts }) => {
+const LoadMore = styled.div`
+  text-align: start;
+  margin-right: 0.2rem;
+`;
+
+const Posts = ({
+  title = false,
+  showLimit = 5,
+  showLoadMore = true,
+  loadMoreTitle = 'Load more',
+  posts,
+}) => {
+  const [limit, setLimit] = useState(showLimit);
+  const visiblePosts = posts.slice(0, limit || posts.length);
+
   return (
     <>
       {title && <h2 className="general-title">Posts</h2>}
-      {posts.map(({ node }) => (
+      {visiblePosts.map(({ node }) => (
         <PostPreview node={node} key={node.fields.slug} />
       ))}
+      {showLoadMore && visiblePosts.length < posts.length && (
+        <LoadMore>
+          <Button
+            variant="link"
+            onClick={() => setLimit((limit) => limit + showLimit)}
+          >
+            {loadMoreTitle}
+          </Button>
+        </LoadMore>
+      )}
       {posts.length === 0 && <div>...</div>}
     </>
   );
