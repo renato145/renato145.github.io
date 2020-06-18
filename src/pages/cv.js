ae90eba89a83db7675db792655df82c92f63a997
Renato <renato145@hotmail.com>
2020-06-18 19:46:22 +0930

    initial cv template

---
+++ src/pages/cv.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import SEO from '../components/Seo';
+
+const CV = () => {
+  return (
+    <div
+      className="mx-auto bg-white p-8"
+      style={{ width: '21cm', height: '29.7cm' }}
+    >
+      <SEO title="CV" description="My CV" />
+
+      <p>Renato Hermoza Aragonés</p>
+      <div>mail / location / git</div>
+
+      <div>
+        <p>Education</p>
+        <p>...</p>
+      </div>
+
+      <div>
+        <p>Resume</p>
+        <p>...</p>
+      </div>
+
+      <div>
+        <p>Experience</p>
+        <p>...</p>
+      </div>
+
+      <div>
+        <p>Knowledge</p>
+        <p>...</p>
+      </div>
+
+      <div>
+        <p>More things</p>
+        <p>...</p>
+      </div>
+    </div>
+  );
+};
+
+export default CV;
