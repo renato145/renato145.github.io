c7972912d961cdc4148bbf87f5c66b27e0155b16
Renato <renato145@hotmail.com>
2020-07-07 01:41:19 +0930

    fixes

--- gatsby-node.js
+++ gatsby-node.js
@@ -96,30 +96,3 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     });
   }
 };
-
-exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
-  const wasmExtensionRegExp = /\.wasm$/;
-
-  const config = getConfig();
-
-  config.resolve.extensions.push('.wasm');
-
-  config.module.rules.forEach((rule) => {
-    (rule.oneOf || []).forEach((oneOf) => {
-      if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
-        // Make file-loader ignore WASM files
-        oneOf.exclude.push(wasmExtensionRegExp);
-      }
-    });
-  });
-
-  // Add a dedicated loader for WASM
-  config.module.rules.push({
-    test: wasmExtensionRegExp,
-    include: path.resolve(__dirname, 'src'),
-    use: [{ loader: require.resolve('wasm-loader'), options: {} }],
-  });
-
-  // This will completely replace the webpack config with the modified object.
-  actions.replaceWebpackConfig(config);
-};
