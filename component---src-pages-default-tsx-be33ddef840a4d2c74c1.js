03e73d671ffc42b2eef1fca589e315ae08afed53
Renato <renato145@hotmail.com>
2020-10-06 00:37:23 +1030

    Updates

---
+++ component---src-pages-default-tsx-be33ddef840a4d2c74c1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{lIG1:function(t,e,a){"use strict";a.r(e),a.d(e,"pageQuery",(function(){return r}));var n=a("q1tI"),i=a.n(n),o=a("soUV");e.default=function(t){var e=t.data,a=t.location,n=e.site.siteMetadata.title;return i.a.createElement(o.a,{location:a,title:n,description:"some description",headerConfig:{title:"header"}})};var r="3159585216"}}]);
+//# sourceMappingURL=component---src-pages-default-tsx-be33ddef840a4d2c74c1.js.map
