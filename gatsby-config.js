1a6603690f1d93e0a64286cc69ad5035335442f9
Renato <renato145@hotmail.com>
2021-05-22 22:16:25 +0930

    change var name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 // const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 const activeEnv = 'development';
 console.log(`Using environment config: '${activeEnv}'`);
+
 require('dotenv').config({
   path: `.env.${activeEnv}`,
 });
@@ -56,7 +57,7 @@ module.exports = {
           createHttpLink({
             uri: `https://api.github.com/graphql`,
             headers: {
-              Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+              Authorization: `bearer ${process.env.GITHUB_REPOS_TOKEN}`,
             },
             fetch,
           }),
