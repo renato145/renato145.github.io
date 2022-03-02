600030f4a88e71459d765987a2e75b9093648c2a
Renato <renato145@hotmail.com>
2022-03-03 00:06:06 +1030

    change gatsby-node to ts

---
+++ gatsby-config.ts
@@ -0,0 +1,147 @@
+import { GatsbyConfig } from 'gatsby';
+import path from "path";
+
+require('dotenv').config({
+  path: '.env',
+});
+
+const fetch = require(`node-fetch`);
+const { createHttpLink } = require(`apollo-link-http`);
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Renato Hermoza`,
+    author: `Renato Hermoza`,
+    shortName: `Renato`,
+    mail: `renato.hermoza@pucp.edu.pe`,
+    description: `Personal portfolio | Blog`,
+    siteUrl: `https://renato145.github.io`,
+    git: `renato145/renato145.github.io`,
+    social: {
+      twitter: `renato145`,
+      github: `renato145`,
+      linkedin: `lala`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.resolve(`src/blog`),
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.resolve(`content/assets`),
+        name: `assets`,
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.resolve(`src/data`),
+      },
+    },
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        fieldName: `github`,
+        typeName: `GitHub`,
+        createLink: () =>
+          createHttpLink({
+            uri: `https://api.github.com/graphql`,
+            headers: {
+              Authorization: `bearer ${process.env.GITHUB_REPOS_TOKEN}`,
+            },
+            fetch,
+          }),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `UA-119996617-1`,
+    //   },
+    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Renato Hermoza`,
+        short_name: `Renato`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
+      options: {
+        // Fields to index
+        fields: [`title`, `tags`, `description`],
+        // How to resolve each field`s value for a supported node type
+        resolvers: {
+          // For any node of type MarkdownRemark, list how to resolve the fields` values
+          Mdx: {
+            title: (node) => node.frontmatter.title,
+            tags: (node) => node.frontmatter.tags,
+            description: (node) => node.frontmatter.description,
+            slug: (node) => node.fields.slug,
+          },
+          GitReposJson: {
+            title: (node) => (node.showName ? node.showName : node.name),
+            name: (node) => node.name,
+            tags: (node) => node.tags,
+          },
+        },
+        // Optional filter to limit indexed nodes
+        // filter: (node, getNode) => node.frontmatter.tags !== 'exempt',
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-sitemap`,
+    // {
+    //   resolve: `gatsby-plugin-typography`,
+    //   options: {
+    //     pathToConfigModule: `src/utils/typography`,
+    //   },
+    // },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
+
+export default config;
---
+++ src/Types.tsx
@@ -0,0 +1,157 @@
+export type GraphqlSite = {
+  site: {
+    siteMetadata: {
+      author: string;
+      description: string;
+      git: string;
+      mail: string;
+      shortName: string;
+      siteUrl: string;
+      social: {
+        github: string;
+        linkedin: string;
+        twitter: string;
+      };
+      title: string;
+    };
+  };
+};
+
+export type GraphqlRepo = {
+  name: string;
+  showName?: string;
+  tags: string[];
+};
+
+export type GraphqlRepos = {
+  allGitReposJson: {
+    nodes: GraphqlRepo[];
+  };
+};
+
+export type GraphqlGit = {
+  name: string;
+  homepageUrl: string | null;
+  url: string;
+  updatedAt: string;
+  openGraphImageUrl: string;
+  description: string | null;
+};
+
+export type GraphqlGitAll = {
+  github: {
+    repositoryOwner: {
+      repositories: {
+        nodes: GraphqlGit[];
+      };
+    };
+  };
+};
+
+export type GraphqlGithubAll = GraphqlRepos & GraphqlGitAll;
+
+export type RepoInfo = GraphqlGit &
+  GraphqlRepo & {
+    imgUrl?: string;
+  };
+
+export type MdxNode = {
+  fields: {
+    slug: string;
+  };
+  frontmatter: {
+    description: string;
+    tags: string[];
+    title: string;
+    date: string;
+  };
+  headings: {
+    depth: number;
+    value: string;
+  };
+  excerpt: string;
+  tableOfContents: {
+    items: {
+      url: string;
+      title: string;
+    }[];
+  };
+  timeToRead: number;
+  wordCount: {
+    paragraphs: number;
+    sentences: number;
+    words: number;
+  };
+};
+
+export type NodesOf<T> = {
+  node: T;
+}[];
+
+export type MdxAllNodes = {
+  allMdx: {
+    edges: NodesOf<MdxNode>;
+  };
+};
+
+export type NewsItem = {
+  date: string;
+  text: string;
+};
+
+export type NewsAll = {
+  allNewsJson: { nodes: NewsItem[] };
+};
+
+export type PresentationsAll = {
+  allPresentationsJson: { nodes: NewsItem[] };
+};
+
+export type Publication = {
+  authors: string;
+  conference: string;
+  doi: string;
+  title: string;
+  year: number;
+};
+
+export type PublicationsAll = {
+  allPublicationsJson: { nodes: Publication[] };
+};
+
+export type Experience = {
+  place: string;
+  position: string;
+  country: string;
+  dateIn: string;
+  dateOut: string;
+  details: string[];
+};
+
+export type ExperienceAll = {
+  allExperienceJson: { nodes: Experience[] };
+};
+
+export type Teaching = {
+  place: string;
+  dateIn: string;
+  dateOut: string;
+  details: string[];
+};
+
+export type TeachingAll = {
+  allTeachingJson: { nodes: Teaching[] };
+};
+
+export type Education = {
+  university: string;
+  degree: string;
+  yearIn: number;
+  yearOut: number;
+  country: string;
+  pending?: boolean;
+};
+
+export type EducationAll = {
+  allEducationJson: { nodes: Education[] };
+};
